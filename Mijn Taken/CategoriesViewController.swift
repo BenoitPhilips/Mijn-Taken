08b01ca0308084f7a880f5df6a6d7917c860e28a
BenoitPhilips <Benoit.Philips@GMail.com>
2019-04-11 11:47:16 +0700

    Added new Categories Table View Controller on the Main.storyboard Back button not present yet

---
+++ Mijn Taken/CategoriesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoriesViewController.swift
+//  Mijn Taken
+//
+//  Created by Benoit Philips on 11/04/2019.
+//  Copyright © 2019 HumbeekWave. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     }
+
+
+    @IBAction func NieuweCatBtnPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
