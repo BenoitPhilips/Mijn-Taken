b3fca59376303b7a06e27f93b413e1ba26f31e74
BenoitPhilips <Benoit.Philips@GMail.com>
2019-04-17 09:36:53 +0700

    Installed Realm swift pods

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Mijn Taken' do
+
+use_frameworks!
+
+# Pods for Mijn Taken
+pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-Mijn Taken/Pods-Mijn Taken-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mijn_Taken : NSObject
+@end
+@implementation PodsDummy_Pods_Mijn_Taken
+@end
